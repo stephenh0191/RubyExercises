dedcfc1d63115fe0c0570dae3a73c342cf83d9cc
stephenh0191 <stephenhughes0191@gmail.com>
2020-10-27 17:15:04 +0000

    Exercises

---
+++ analyziser.rb
@@ -0,0 +1,7 @@
+puts "simple calculator"
+25.times {print "-"}
+puts "enter the first numner"
+num_1 = gets.chomp
+puts "enter second number"
+num_2 = get.chomp
+puts " the first number mutiplied by the second number is #{num_1 * num_2}"
